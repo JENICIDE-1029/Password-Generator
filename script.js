370f7969325d79b0c91a013af5ad1b4902c0904b
[Jen ny] <[jengutierrez.1210@gmail.com]>
2022-11-28 21:23:43 -0800

    i went ahead and met with Steven, a tutor, and he helped me with establishing my two functions that are used for defining the password criteria which have loops contained within

--- script.js
+++ script.js
@@ -4,8 +4,6 @@
 var upperCaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 var lowerCaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 var numbersArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-//i dont remeber what all the special characters are used in the example but this is ENOUGH and will still provide the same 
-//functionality just different characters, KAY
 var specialCharArray = ["!", "@", "#", "$", "%", "^", "&", "*", "-", "?", "/"]
 
 //if im understanding correctly we are going to add these characters of these arrays into a much bigger array
@@ -19,6 +17,7 @@ function firstSetPWCriteria() {
     //this alert will only need to show once
     alert("Welcome to the password generator, please select one of the following four options and then enter a length for your final password");
     
+    //this is inputting the result into a variable while also displaying it to users
     var upperConfirm = confirm("Would you like to add upper case letters? ");
     var lowerConfirm = confirm("Would you like to add lower case letters? ");
     var specialConfirm = confirm("Would you like to add special symbols? ");
@@ -27,34 +26,58 @@ function firstSetPWCriteria() {
 
     //so starting here is where my loop is going to start and where users will get thrown back into if they didnt select anything
     if (upperConfirm == false && lowerConfirm == false && specialConfirm == false && numberConfirm == false) {
-    //  alert("Oops, it looks like you didn't select any of the four criteria, PLEASE select at least one! ");
-        
-    //         confirm("Would you like to add upper case letters? ");
-    //         confirm("Would you like to add lower case letters? ");
-    //         confirm("Would you like to add special symbols? ");
-    //         confirm("Would you like to add numbers? ");
 
-        do {
+        while (upperConfirm == false && lowerConfirm == false && specialConfirm == false && numberConfirm == false) {
+           
             alert("Oops, it looks like you didn't select any of the four criteria, PLEASE select at least one! ");
         
-            confirm("Would you like to add upper case letters? ");
-            confirm("Would you like to add lower case letters? ");
-            confirm("Would you like to add special symbols? ");
-            confirm("Would you like to add numbers? ");
+            var upperConfirm = confirm("Would you like to add upper case letters? ");
+            var lowerConfirm = confirm("Would you like to add lower case letters? ");
+           var specialConfirm = confirm("Would you like to add special symbols? ");
+            var numberConfirm = confirm("Would you like to add numbers? ");
             //this is what's going to prevent an infinity loop and break us out once any of the criteria are selected
             if (upperConfirm == true || lowerConfirm == true || specialConfirm == true || numberConfirm == true){
                 break;
             }
             
-        } while (!upperConfirm && !lowerConfirm && !specialConfirm && !numberConfirm )
+        } 
     } 
     
 
 }
 
+//in this section is where we define the length of our password
+function PWLengthCriteria (){
+    //we need to ensure that what is entered is between 8 and 128 characters and that there is no letters used
+    var passwordLength = prompt("Enter your desired password length from 8-128 characters, make sure to only type numbers!");
+
+    if (passwordLength == "" || passwordLength < 7 || passwordLength > 129 || isNaN(passwordLength)) {
+        alert("Listen buddy, I said to enter in a number from 8-128 and to only use numbers! ")
+        while (passwordLength == "" || passwordLength < 7 || passwordLength > 129 || isNaN(passwordLength)) {
+            prompt("Enter your desired password length from 8-128 characters, make sure to only type numbers!");
+                if (passwordLength < 7 || passwordLength > 129) {
+                    break;
+                }
+
+        }
+    } 
+
+}
+
+PWLengthCriteria.passwordLength
+//as of rn this function is not allowing me to loop through, ovbi, but at least the error is going through 
+//there is also an error where no matter what i enter i am getting the error message
 
 
-//because if an item is selected then we'd like for our generated password to include that criteria
+//in this section is where i need to actually have the password generate
+//i believe i have to create an array of which will get all of the characters specified in our previous functions 
+//this is when we use math.floor and math.random multiplied by the length specified from our user
+
+
+
+//in this section we will fire off all of our functions
+firstSetPWCriteria();
+PWLengthCriteria();
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -65,9 +88,8 @@ function writePassword() {
     var passwordText = document.querySelector("#password");
 
     passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-firstSetPWCriteria();
+
